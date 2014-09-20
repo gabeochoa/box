bffd0d31ce5a3531fe6672a8417d8cf764066971
Gabriel Ochoa <gabe1118@yahoo.com>
2014-09-20 09:05:24 -0400

    Network Lobbies!
    added lobbies
    added player model

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class NetworkManager : MonoBehaviour {
+	public GameObject loadcam;
+
+	public bool offlineMode = false;
+	bool connecting = false;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.player.name = PlayerPrefs.GetString("Username", "Guy");;
+	}
+	
+	void OnDestroy() {
+		PlayerPrefs.SetString("Username", PhotonNetwork.player.name);
+	}
+
+	void Connect() {
+		PhotonNetwork.ConnectUsingSettings( "dkbox" );
+	}
+	
+	void OnGUI() {
+		GUILayout.Label( PhotonNetwork.connectionStateDetailed.ToString() );
+		
+		if (PhotonNetwork.connected == false && connecting == false) {
+						GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
+						GUILayout.BeginHorizontal ();
+						GUILayout.FlexibleSpace ();
+						GUILayout.BeginVertical ();
+						GUILayout.FlexibleSpace ();
+			
+						GUILayout.BeginHorizontal ();
+						GUILayout.Label ("Username: ");
+						PhotonNetwork.player.name = GUILayout.TextField (PhotonNetwork.player.name);
+						GUILayout.EndHorizontal ();
+			
+						/*
+			if( GUILayout.Button("Single Player") ) {
+				connecting = true;
+				PhotonNetwork.offlineMode = true;
+				OnJoinedLobby();
+			}
+			*/
+			
+						if (GUILayout.Button ("Multi Player")) {
+								connecting = true;
+								Connect ();
+						}
+						GUILayout.FlexibleSpace ();
+						GUILayout.EndVertical ();
+						GUILayout.FlexibleSpace ();
+						GUILayout.EndHorizontal ();
+						GUILayout.EndArea ();
+				}
+	}
+	
+	void OnJoinedLobby() {
+		Debug.Log ("OnJoinedLobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+	
+	void OnPhotonRandomJoinFailed() {
+		Debug.Log ("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom( null );
+	}
+	
+	void OnJoinedRoom() {
+		Debug.Log ("OnJoinedRoom");
+		
+		connecting = false;
+		SpawnMyPlayer();
+	}
+	
+	void SpawnMyPlayer() {
+		Debug.Log("Spawning player: " + PhotonNetwork.player.name);
+
+		GameObject myPlayerGO = (GameObject)PhotonNetwork.Instantiate("Ali", Vector3.zero,Quaternion.identity,0);
+		loadcam.SetActive(false);
+		myPlayerGO.transform.FindChild("OVRCameraController").gameObject.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/NetworkMvt.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NetworkMvt : Photon.MonoBehaviour {
+	
+	Vector3 realPosition = Vector3.zero;
+	Quaternion realRotation = Quaternion.identity;
+	
+	bool gotFirstUpdate = false;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( photonView.isMine ) {
+			// Do nothing -- the character motor/input/etc... is moving us
+		}
+		else {
+			transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+			transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+		}
+	}
+	
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		if(stream.isWriting) {
+			// This is OUR player. We need to send our actual position to the network.
+			
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		}
+		else {
+			// This is someone else's player. We need to receive their position (as of a few
+			// millisecond ago, and update our version of that player.
+			
+			// Right now, "realPosition" holds the other person's position at the LAST frame.
+			// Instead of simply updating "realPosition" and continuing to lerp,
+			// we MAY want to set our transform.position to immediately to this old "realPosition"
+			// and then update realPosition
+			
+			
+			realPosition = (Vector3)stream.ReceiveNext();
+			realRotation = (Quaternion)stream.ReceiveNext();
+			
+			if(gotFirstUpdate == false) {
+				transform.position = realPosition;
+				transform.rotation = realRotation;
+				gotFirstUpdate = true;
+			}
+			
+		}
+		
+	}
+}
